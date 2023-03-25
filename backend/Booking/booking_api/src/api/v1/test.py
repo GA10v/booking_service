e0ea1d0f41462d1e34118151d1064f9cdeb9aaed
Galovach Andrey <a.galovach@yandex.ru>
2023-03-25 05:13:05 +0300

    переделал тестовую ручку

--- backend/Booking/booking_api/src/api/v1/test.py
+++ backend/Booking/booking_api/src/api/v1/test.py
@@ -1,23 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
 
 @router.post(
-    '/test_ok',
-    summary='Test OK',
-    description='Тест OK',
+    '/ping',
+    summary='Test ping',
+    description='Тест ping',
 )
 async def test_ok(request: Request) -> HTTPStatus:
     return HTTPStatus.OK
-
-
-@router.post(
-    '/test_bad',
-    summary='Test UNAUTHORIZED',
-    description='Тест UNAUTHORIZED',
-)
-async def test_bad(request: Request) -> HTTPStatus:
-    raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail='test')
