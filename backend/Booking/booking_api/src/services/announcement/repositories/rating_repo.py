7c76feafc51368e93c3c48fb3d5dca5eab0cb3c9
Galovach Andrey <a.galovach@yandex.ru>
2023-03-31 21:28:53 +0300

    RatingMockRepository

--- backend/Booking/booking_api/src/services/announcement/repositories/rating_repo.py
+++ backend/Booking/booking_api/src/services/announcement/repositories/rating_repo.py
@@ -1,6 +1,19 @@
+import random
+from uuid import UUID
+
+from core.logger import get_logger
 from services.announcement.repositories import _protocols
+
+logger = get_logger(__name__)
 
 
 class RatingMockRepository(_protocols.RatingRepositoryProtocol):
     def __init__(self) -> None:
-        ...
+        logger.info('RatingMockRepository init ...')
+
+    async def get_by_id(self, user_id: str | UUID) -> float:
+        return random.uniform(0.0, 10.0)
+
+
+def get_rating_repo() -> _protocols.RatingRepositoryProtocol:
+    return RatingMockRepository()
