500b12d1a2b7c6ac5bc21bddb7f0fa95e9f7d828
Захаров Павел <pzakharov83@gmail.com>
2023-04-09 16:59:30 +0300

    add logging

--- backend/Booking/review_api/service/mongo_driver.py
+++ backend/Booking/review_api/service/mongo_driver.py
@@ -10,7 +10,7 @@ class MongoService:
     def __init__(self):
         self.client = motor_asyncio.AsyncIOMotorClient(settings.mongo.uri)
         self.database = self.client[settings.mongo.DB]
-        self.collection = self.database[self.collection]
+        self.collection = self.database[settings.mongo.COLLECTION]
 
     async def insert_document(self, _doc: Review):
         await self.collection.insert_one(_doc.json())
