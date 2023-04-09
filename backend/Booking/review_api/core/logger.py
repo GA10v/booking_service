4d8350621942253cb6543c43bc7a0d693ce5474f
Захаров Павел <pzakharov83@gmail.com>
2023-04-09 14:12:34 +0300

    turn down logging

--- backend/Booking/review_api/core/logger.py
+++ backend/Booking/review_api/core/logger.py
@@ -68,7 +68,7 @@ LOGGING = {
         'handlers': LOG_DEFAULT_HANDLERS,
     },
 }
-logging.config.dictConfig(LOGGING)
+# logging.config.dictConfig(LOGGING)
 
 
 def get_logger(_name_: str) -> logging.Logger:
