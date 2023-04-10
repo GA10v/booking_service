5ee7bdc4cdf64b8b0596de11aa32b1027e43b35a
Захаров Павел <pzakharov83@gmail.com>
2023-04-10 17:06:47 +0300

    change logic

--- backend/Booking/review_api/service/review.py
+++ backend/Booking/review_api/service/review.py
@@ -9,6 +9,7 @@ from db.redis_storage import get_redis, ReviewCollection
 
 REDIS_INSTANCE = Depends(get_redis)
 MONGO_INSTANCE = Depends(get_mongo)
+RESULTS_COUNT = 100
 
 
 class ReviewService:
@@ -34,7 +35,7 @@ class ReviewService:
     async def get_all_reviews_for_event_id(self, event_id: str):
         result = await self.redis.get_document_by_event_id(event_id)
         if not result:
-            result = ReviewCollection(list(await self.mongo.get_document_by_event_id(event_id)))
+            result = ReviewCollection(await self.mongo.get_document_by_event_id(event_id).to_list(length=RESULTS_COUNT))
         if result:
             await self.redis.put_reviews_to_cache(event_id, result)
         return [Review.parse_obj(review) async for review in result]
