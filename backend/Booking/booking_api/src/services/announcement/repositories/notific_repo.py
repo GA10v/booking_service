100358cb27af19e79be23bca28fa774bfcf7fcd8
Захаров Павел <pzakharov83@gmail.com>
2023-04-13 17:59:31 +0300

    fix ssl

--- backend/Booking/booking_api/src/services/announcement/repositories/notific_repo.py
+++ backend/Booking/booking_api/src/services/announcement/repositories/notific_repo.py
@@ -35,6 +35,7 @@ class NotificApiRepository(_protocols.NotificRepositoryProtocol):
                 self.notific_endpoint,
                 headers=self._headers,
                 json=event,
+                ssl_context=None,
             ) as resp:
                 logger.info(f'Send notific <{self.notific_endpoint}>')
                 logger.info(f'Send notific resp.status: <{resp.status}>')
